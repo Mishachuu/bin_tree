0efac78a757ddf79b8d58b3286796cf1e224e9c6
Миша Чукарев <mishakrut11@gmail.com>
2023-03-08 00:17:37 +0400

    add func insert and contains

--- Project8/main.cpp
+++ Project8/main.cpp
@@ -12,3 +12,38 @@ bool erase(int key) Ц удаление элемента;
 
 */
 using namespace std;
+class Set {
+private:
+	struct bin_tree
+	{
+		int value;
+		bin_tree* left;
+		bin_tree* right;
+		bin_tree(int val) : value(val), left(nullptr), right(nullptr){}
+	};
+	bin_tree* root;
+	bool insert(bin_tree* tree, int val) {
+		if (!tree) {
+			tree = new bin_tree(val);
+			return true;
+		}
+		if (val < tree->value) {
+			insert(tree->left, val);
+		}
+		else if (val > tree->value) {
+			 insert(tree->right, val);
+		}
+		return true;
+	}
+	bool contains(bin_tree* tree, int key) {
+		if (!tree) return false;
+		if (key > tree->value) {
+			return contains(tree->right, key);
+		}
+		else if (key < tree->value) {
+			return insert(tree->left, key);
+			}
+		return true;
+	}
+
+};
